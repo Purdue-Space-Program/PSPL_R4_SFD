cb33c0accdf5be57d717396b342fa0574c64447e
Nickolas Nielsen <nielsenn@purdue.edu>
2024-12-18 20:20:20 -0600

    Setup

---
+++ constants.py
@@ -0,0 +1,42 @@
+## Mass
+
+LB2KG = 0.453592  # [kg/lbm] Conversion factor from lbm to kg
+KG2LB = 1 / LB2KG  # [lbm/kg] Conversion factor from kg to lbm
+
+G2KG = 0.001  # [kg/g] Conversion factor from g to kg
+KG2G = 1 / G2KG  # [g/kg] Conversion factor from kg to g
+
+## Length
+
+IN2M = 0.0254  # [m/in] Conversion factor from in to m
+M2IN = 1 / IN2M  # [in/m] Conversion factor from m to in
+
+M2FT = 3.28084  # [ft/m] Conversion factor from m to ft
+FT2M = 1 / M2FT  # [m/ft] Conversion factor from ft to m
+
+## Area
+
+IN22M2 = 0.0064516  # [m^2/in^2] Conversion factor from in^2 to m^2
+M22IN2 = 1 / IN22M2  # [in^2/m^2] Conversion factor from m^2 to in^2
+
+## Volume
+
+L2M3 = 0.001  # [m^3/l] Conversion factor from L to m^3
+M32L = 1 / L2M3  # [1/m^3] Conversion factor from m^3 to L
+
+M32IN3 = 61023.7  # [in^3/m^3] Conversion factor from m^3 to in^3
+IN32M3 = 1 / M32IN3  # [m^3/in^3] Conversion factor from in^3 to m^3
+
+M32FT3 = 35.3147  # [ft^3/m^3] Conversion factor from m^3 to ft^3
+FT32M3 = 1 / M32FT3  # [m^3/ft^3] Conversion factor from ft^3 to m^3
+
+## Force
+
+N2LBF = 0.224809  # [lbf/N] Conversion factor from N to lbf
+LBF2N = 1 / N2LBF  # [N/lbf] Conversion factor from lbf to N
+
+
+# FAR Constants
+
+FAR_ALTITUDE = 615.09  # [m] altitude of FAR launch site
+RAIL_HEIGHT = 18.29  # [m] height of the rail
